fef91f8be2b5f7f6e3aae27b0e593efec1962777
Kate1jkee <voronina1996-1996k@mail.ru>
2026-03-13 14:20:42 +0300

    Правки после комментариев наставника

--- js/main.js
+++ js/main.js
@@ -54,6 +54,7 @@ const getRandomPositivInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
+
   return Math.floor(result);
 };
 
@@ -63,10 +64,14 @@ const getRandomArrayElement = (elements) =>
 // эта функция увеличивает значение переменной counter на 1
 const increasByOne = function (min, max) {
   let counter = min;
+
   return function () {
-    if (counter <= max) {
+    if (max === undefined) {
+      counter += 1;
+    } else if (counter <= max) {
       counter += 1;
     }
+
     return counter;
   };
 };
@@ -77,29 +82,48 @@ const choseMessage = function () {
     getRandomArrayElement(MESSAGE),
     getRandomArrayElement(MESSAGE),
   ];
+
   const countMessage = getRandomPositivInteger(1, 2);
+
   if (countMessage === 1) {
     return messages[0];
   } else {
     while (messages[0] === messages[1]) {
       messages[1] = getRandomArrayElement(MESSAGE); // проверка на совпадение 2х случайных комментариев, елси совпали выбери новый
     }
-    return messages[0] + messages[1];
+
+    return `${messages[0]} ${messages[1]}`;
   }
 };
 
-// объект одного комментария он скорее всего должне быть одной функцией и все что выше просто в нее добавляем вопрос какие аргументы?
+//Функция создает объект комментария
+const createComment = function (countIdComment) {
+  return {
+    id: countIdComment(),
+    avatar: `img/avatar-${getRandomPositivInteger(1, 6)}.svg`,
+    message: choseMessage(),
+    name: getRandomArrayElement(NAME),
+  };
+};
+
+//Функция собирает все объекты комментариев в массив
 const generateComments = function (countIdComment) {
   const countComment = getRandomPositivInteger(0, 30);
-  const createComment = function () {
-    return {
-      id: countIdComment(),
-      avatar: `img/avatar-${getRandomPositivInteger(1, 6)}.svg.`,
-      message: choseMessage(),
-      name: getRandomArrayElement(NAME),
-    };
+
+  return Array.from({ length: countComment }, () =>
+    createComment(countIdComment),
+  );
+};
+
+//Функция создает объект публикации
+const createObject = function (countUrl, countId, countIdComment) {
+  return {
+    id: countId(),
+    url: `photos/${countUrl()}.jpg`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomPositivInteger(15, 200),
+    comments: generateComments(countIdComment),
   };
-  return Array.from({ length: countComment }, createComment);
 };
 
 // Функция которая формирует массив из объектов публикаций
@@ -107,16 +131,10 @@ const generateObjects = function () {
   const countUrl = increasByOne(0, 25);
   const countId = increasByOne(0, 25);
   const countIdComment = increasByOne(0);
-  const createObject = function () {
-    return {
-      id: countId(),
-      url: `photos/${countUrl()}.jpg`,
-      description: getRandomArrayElement(DESCRIPTION),
-      likes: getRandomPositivInteger(15, 200),
-      comments: generateComments(countIdComment),
-    };
-  };
-  return Array.from({ length: 25 }, createObject);
+
+  return Array.from({ length: 25 }, () =>
+    createObject(countUrl, countId, countIdComment),
+  );
 };
 
 generateObjects();
