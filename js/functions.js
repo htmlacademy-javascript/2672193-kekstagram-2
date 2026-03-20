0bb37297b30a7f7480791785db30a5b962ac7590
Kate1jkee <voronina1996-1996k@mail.ru>
2026-03-20 14:12:02 +0300

    Задание 5.15, строка 41

--- js/functions.js
+++ js/functions.js
@@ -37,3 +37,27 @@ console.log(checkPalindrome('Лёша на полке клопа нашёл '));
 console.log(checkPalindrome('Привет, мир!')); // false
 console.log(checkPalindrome('Февраль')); // false
 */
+
+//задание 5.16.
+//функция переводит строчную запись времени часы минуты в число минут
+function transformToMinutes(time) {
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
+//функция проверят не выходит ли начало и конец встречи за рамки начала и конца рабочего для
+function isMeetingOnWorkDay(timeStartWorkDay, timeEndWorkDay, timeStartMeeting, durationMeeting) {
+  const startWork = transformToMinutes(timeStartWorkDay); //480
+  const endWork = transformToMinutes(timeEndWorkDay); //1050
+  const startMeeting = transformToMinutes(timeStartMeeting); //840
+
+  const endMeeting = startMeeting + durationMeeting; //930
+
+  return startMeeting >= startWork && endMeeting <= endWork;
+}//возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит.
+
+isMeetingOnWorkDay('08:00', '17:30', '14:00', 90); // true
+isMeetingOnWorkDay('8:0', '10:0', '8:0', 120); // true
+isMeetingOnWorkDay('08:00', '14:30', '14:00', 90); // false
+isMeetingOnWorkDay('14:00', '17:30', '08:0', 90); // false
+isMeetingOnWorkDay('8:00', '17:30', '08:00', 900); // false
