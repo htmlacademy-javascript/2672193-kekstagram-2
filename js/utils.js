f5710c81a36543e3f0600bd60278eb5be71176d6
Kate1jkee <voronina1996-1996k@mail.ru>
2026-03-31 20:43:49 +0300

    JSDoc-комментарии

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,9 @@
-// эта функция вернет число из диапозона а и b
+/**
+ * Эта функция вернет число из диапозона а и b
+ * @param {*} a Минимальное значение интервала.
+ * @param {*} b Максимальное значение интервала.
+ * @returns Рандомное число из диапозона.
+ */
 const getRandomPositivInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -7,6 +12,11 @@ const getRandomPositivInteger = (a, b) => {
   return Math.floor(result);
 };
 
+/**
+ * Эта функция вернет число, которое будет соответствовать индексу элемента массива.
+ * @param {*} elements Название массива, от куда будем брать элемент.
+ * @returns Элементы из заданного массива.
+ */
 const getRandomArrayElement = (elements) =>
   elements[getRandomPositivInteger(0, elements.length - 1)];
 
